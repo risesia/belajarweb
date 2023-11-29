db83fa938eb7aa9fe3e3887d32fc4d1a8e6d1046
Rizky <rizkyterm@gmail.com>
2023-11-29 22:46:23 +0700

    finishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,44 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://risesia.github.io',
+	base: '/belajarweb',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Belajar Web Dev',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+					label: 'Panduan Pembelajaran',
+					link: './panduan'
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'Materi & Latihan',
+					items: [
+						{
+							label: 'Pendahuluan',
+							link: './pendahuluan'
+						},
+						{
+							label: 'Pengenalan HTML',
+							link: './pengenalan-html'
+						},
+						{
+							label: 'Format Teks HTML',
+							link: './format-html'
+						},
+						{
+							label: 'List pada HTML',
+							link: './list-html'
+						},
+						{
+							label: 'List kombinasi',
+							link: './list-kombinasi-html'
+						}
+					]
+				}
 			],
 		}),
 	],
